11d882d3b250965077a66598fd79d175d07127f5
Benjamin Akhras <b@akhras.at>
2024-11-19 10:26:59 +0100

    cleanup APT groups

--- apt_groups.py
+++ apt_groups.py
@@ -2,55 +2,36 @@ import requests
 import csv
 import json
 
-BASE_URL = "https://docs.google.com/spreadsheets/d/1H9_xaxQHpWaa4O_Son4Gx0YOIzlcBWMsdvePFX68EKU/export?format=csv&gid="
-
-gids = [
-    361554658,
-    1636225066,
-    1905351590,
-    376438690,
-    300065512,
-    2069598202,
-    574287636,
-    438782970,
-]
+BASE_URL = "https://docs.google.com/spreadsheets/d/1H9_xaxQHpWaa4O_Son4Gx0YOIzlcBWMsdvePFX68EKU/export?format=csv&gid=856560690"
 
 
 def extract_apt_groups() -> dict:
     all_data = {}
 
-    for gid in gids:
-        try:
-            response = requests.get(BASE_URL + str(gid))
-            response.raise_for_status()
-        except requests.RequestException as e:
-            print(f"Failed to fetch data for gid {gid}. Error: {e}")
+    try:
+        response = requests.get(BASE_URL)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        print(f"Failed to fetch data for gid {BASE_URL}. Error: {e}")
+
+    lines = response.content.decode("utf-8").splitlines()
+
+    # Set headers for DictReader
+    headers = lines[2].split(",")
+
+    csv_data = lines[3:]  # Data starting from the fourth line
+
+    reader = csv.DictReader(csv_data, fieldnames=headers)
+
+    # Extract "Common Name" and "Country" columns
+    for row in reader:
+        common_name = row.get("New name", None)
+        country = row.get("Origin/Threat", None)
+        if not common_name or not country:
             continue
-
-        lines = response.content.decode("utf-8").splitlines()
-
-        # Extract the country name from the first line
-        country = lines[0].split(",")[0]
-
-        # Set headers for DictReader
-        headers = lines[1].split(",")
-
-        csv_data = lines[2:]  # Data starting from the third line
-
-        reader = csv.DictReader(csv_data, fieldnames=headers)
-        country_data = []
-
-        # Extract "Common Name" and "CrowdStrike" columns
-        for row in reader:
-            common_name = row.get("Common Name", None)
-            crowdstrike = row.get("CrowdStrike", None)
-
-            if common_name:
-                country_data.append(common_name)
-            if crowdstrike:
-                country_data.append(crowdstrike)
-
-        all_data[country] = sorted(list(set(country_data)))  # Deduplication per country
+        if country not in all_data:
+            all_data[country] = []
+        all_data[country].append(common_name)
 
     return all_data
 
@@ -59,10 +40,8 @@ def get_apt():
     apt_groups_by_country = extract_apt_groups()
 
     entries = []
-    for country, groups in apt_groups_by_country.items():
-        for group in groups:
-            if "?" in group or "Unknown" in group:
-                continue
+    for country in apt_groups_by_country:
+        for group in apt_groups_by_country[country]:
             entry = {"value": group, "category": "APT", "description": f"{country} APT"}
             entries.append(entry)
 
